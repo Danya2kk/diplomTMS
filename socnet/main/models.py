58f49b4102268ffbe4c65dbe9b3362d43137e1a4
saatarko <saatar@tut.by>
2024-08-12 20:38:11 +0300

    create migration

--- socnet/main/models.py
+++ socnet/main/models.py
@@ -9,20 +9,18 @@ from django.contrib.auth.models import AbstractUser, Group, Permission
 class User(AbstractUser):
     created_at = models.DateTimeField(auto_now_add=True)
 
-    class User(AbstractUser):
-        created_at = models.DateTimeField(auto_now_add=True)
+    groups = models.ManyToManyField(
+        Group,
+        related_name='custom_user_groups',
+        blank=True
+    )
 
-        groups = models.ManyToManyField(
-            Group,
-            related_name='custom_user_groups',
-            blank=True
-        )
+    user_permissions = models.ManyToManyField(
+        Permission,
+        related_name='custom_user_permissions',
+        blank=True
+    )
 
-        user_permissions = models.ManyToManyField(
-            Permission,
-            related_name='custom_user_permissions',
-            blank=True
-        )
 
 
 
