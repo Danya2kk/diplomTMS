9d8b5935f01aa51b0167d2cc2749a15f24aec095
Fedyaslonn <wisp47344@gmail.com>
2024-08-29 21:29:57 +0300

    Add Filters to Friendship, Group, Profile models

--- socnet/main/filters.py
+++ socnet/main/filters.py
@@ -3,9 +3,9 @@ from .models import *
 
 
 class FriendshipFilter(filters.FilterSet):
-    status = filters.MultipleChoiceFilter(choices=Friendship.STATUS_CHOICES)
-    profile_one = filters.NumberFilter(field_name='profile_one', lookup_expr='exact')
-    profile_two = filters.NumberFilter(field_name='profile_two', lookup_expr='exact')
+    status = filters.ModelChoiceFilter(queryset=FriendshipStatus.objects.all())
+    profile_one = filters.ModelChoiceFilter(field_name='profile_one', queryset=Profile.objects.all())
+    profile_two = filters.ModelChoiceFilter(field_name='profile_two', queryset=Profile.objects.all())
 
     class Meta:
         model = Friendship
@@ -16,9 +16,9 @@ class ProfileFilter(filters.FilterSet):
     firstname = filters.CharFilter(field_name='firstname', lookup_expr='icontains')
     lastname = filters.CharFilter(field_name='lastname', lookup_expr='icontains')
     location = filters.CharFilter(field_name='location', lookup_expr='icontains')
-    interests = filters.MultipleChoiceFilter(
+    interests = filters.ModelMultipleChoiceFilter(
         field_name='interests',
-        queryset = Interest.objects.all(),
+        queryset=Interest.objects.all(),
     )
 
     class Meta:
@@ -28,7 +28,7 @@ class ProfileFilter(filters.FilterSet):
 class GroupFilter(filters.FilterSet):
     name = filters.CharFilter(field_name='name', lookup_expr='icontains')
     description = filters.CharFilter(field_name='name', lookup_expr='icontains')
-    group_type = filters.MultipleChoiceFilter(field_name='group_type', choices=Group.GROUP_TYPES)
+    group_type = filters.ChoiceFilter(field_name='group_type', choices=Group.GROUP_TYPES)
 
     class Meta:
         model = Group
