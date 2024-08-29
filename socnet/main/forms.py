c21fe7bedcee5eba7791ad6ce313e110be819809
ViktoriaKonoplyanik <v375447086861@gmail.com>
2024-08-29 18:03:44 +0300

    registr

---
+++ socnet/main/forms.py
@@ -0,0 +1,44 @@
+from django import forms
+from .models import User
+from django import forms
+from django.contrib.auth import authenticate
+
+
+class RegistrationForm(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput)
+    password_confirm = forms.CharField(widget=forms.PasswordInput, label="Confirm Password")
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password']
+
+    def clean(self):
+        cleaned_data = super().clean()
+        password = cleaned_data.get("password")
+        password_confirm = cleaned_data.get("password_confirm")
+
+        if password and password_confirm and password != password_confirm:
+            self.add_error('password_confirm', "Passwords do not match")
+        return cleaned_data
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.set_password(self.cleaned_data["password"])
+        if commit:
+            user.save()
+        return user
+
+
+class LoginForm(forms.Form):
+    username = forms.CharField()
+    password = forms.CharField(widget=forms.PasswordInput)
+
+    def clean(self):
+        cleaned_data = super().clean()
+        username = cleaned_data.get('username')
+        password = cleaned_data.get('password')
+
+        user = authenticate(username=username, password=password)
+        if not user:
+            raise forms.ValidationError("Invalid login or password.")
+        return cleaned_data
