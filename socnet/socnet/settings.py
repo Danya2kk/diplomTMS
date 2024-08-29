3f24fa02f1031e3d001d827384e68ca04720bd69
Saatarko <saatarko@tut.by>
2024-08-29 22:06:52 +0300

    add filters in settings.py

--- socnet/socnet/settings.py
+++ socnet/socnet/settings.py
@@ -53,6 +53,7 @@ INSTALLED_APPS = [
     'main',
     'corsheaders',
     'channels',
+    'django_filters',
 ]
 
 MIDDLEWARE = [
@@ -134,6 +135,7 @@ REST_FRAMEWORK = {
         'rest_framework.renderers.JSONRenderer',
         'rest_framework.renderers.BrowsableAPIRenderer',
     ),
+    'DEFAULT_FILTER_BACKENDS': ('django_filters.rest_framework.DjangoFilterBackend',),
 }
 
 
